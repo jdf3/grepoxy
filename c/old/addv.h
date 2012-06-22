52006f176258e0995281e8fc58d91848f344a90c
Jeremy Fehr <jfehr@wesleyan.edu>
2012-06-22 11:01:46 -0400

    Added glueg.c. addv.c now parses g6, but no longer works correctly.
    cat ../../testfiles/c4k6/c4k6n12.g6 | ./addv 6 | labelg | sort -u | wc
    -l gives 60786, not the correct value of 60789. Similarly, cat
    ../../testfiles/c4k6/c4k6n16.g6 | ./addv 6 | labelg | sort -u | wc -l
    gives 2, not 5...

---
+++ c/old/addv.h
@@ -0,0 +1,16 @@
+#ifndef _ADDV
+#define _ADDV
+
+#define MV 32
+#define MINCLIQUE 4
+#define MAXCLIQUE 6
+#define USAGE "addv forbiddencliquesize"
+
+#define DEBUG 0
+
+extern int num_bytes;
+extern int am[MV][MV];
+extern int v;
+extern int oldv;
+
+#endif
